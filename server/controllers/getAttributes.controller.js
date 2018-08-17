575ee8340c6d2de7bdbd7d34f32061e8bf666111
Nick McCally <nick@amida.com>
2018-08-17 01:09:58 -0400

    getAttributes

--- server/controllers/getAttributes.controller.js
+++ server/controllers/getAttributes.controller.js
@@ -5,6 +5,7 @@ import APIError from '../helpers/APIError';
 
 const Namespace = db.Namespace;
 const Domain = db.Domain;
+const Attribute = db.Attribute;
 const Op = Sequelize.Op;
 
 
@@ -27,10 +28,54 @@ function getDomain(req, res, next) {
 
     const { namespace } = req.params;
 
+    // if(!isNaN(namespace)) {
+    //     Domain.findAll({
+    //         where: { NamespaceId: namespace },
+    //         order: [['id', 'ASC']]
+    //     })
+    //     .then((messages) => {
+    //         if (messages.length === 0) {
+    //             const err = new APIError('There were no results', 'NO_RESULTS', httpStatus.NOT_FOUND, true);
+    //             next(err);
+    //         } else {
+    //             res.send(messages);
+    //         }
+    //     })
+    //     .catch(next);
+    // } else {
+    //     Namespace.findAll({
+    //       where: { uri: namespace },
+    //     }).then((namespace) => {
+    //       if (namespace.length === 0) {
+    //           const err = new APIError('There were no results', 'NO_RESULTS', httpStatus.NOT_FOUND, true);
+    //           next(err);
+    //       } else {
+    //         Domain.findAll({
+    //             where: { NamespaceId: namespace[0].dataValues.id },
+    //             order: [['id','ASC']]
+    //         })
+    //         .then((messages) => {
+    //             if (messages.length === 0) {
+    //                 const err = new APIError('There were no results', 'NO_RESULTS', httpStatus.NOT_FOUND, true);
+    //                 next(err);
+    //             } else {
+    //                 res.send(messages);
+    //             }
+    //         })
+    //         .catch(next);
+    //       }
+    //     })
+    // }
     if(!isNaN(namespace)) {
-        Domain.findAll({
-            where: { NamespaceId: namespace },
-            order: [['id', 'ASC']]
+      Namespace.findAll({
+        attributes: ['id','namespace','uri'],
+        where: { id: namespace },
+          include: [{
+            model: Domain,
+            attributes: ['id','domain','uri'],
+            order: [['id','ASC']],
+          }],
+          order: [[Domain,'id','ASC']],
         })
         .then((messages) => {
             if (messages.length === 0) {
@@ -42,23 +87,25 @@ function getDomain(req, res, next) {
         })
         .catch(next);
     } else {
-        Namespace.findAll({
-          where: { uri: namespace },
-        }).then((namespace) => {
-          Domain.findAll({
-              where: { NamespaceId: namespace[0].dataValues.id },
-              order: [['id','ASC']]
-          })
-          .then((messages) => {
-              if (messages.length === 0) {
-                  const err = new APIError('There were no results', 'NO_RESULTS', httpStatus.NOT_FOUND, true);
-                  next(err);
-              } else {
-                  res.send(messages);
-              }
-          })
-          .catch(next);
+      Namespace.findAll({
+        attributes: ['id','namespace','uri'],
+        where: { uri: namespace },
+
+          include: [{
+            model: Domain,
+            attributes: ['id','domain','uri'],
+            order: [['id','ASC']],
+          }],
+          order: [[Domain,'id','ASC']],
+        }).then((messages) => {
+            if (messages.length === 0) {
+                const err = new APIError('There were no results', 'NO_RESULTS', httpStatus.NOT_FOUND, true);
+                next(err);
+            } else {
+                res.send(messages);
+            }
         })
+        .catch(next);
     }
 }
 
@@ -67,10 +114,26 @@ function getAttribute(req, res, next) {
 
     const { namespace, domain } = req.params;
 
+
+
+
     if(!isNaN(namespace)) {
-        Domain.findAll({
-            where: { NamespaceId: namespace },
-            order: [['id', 'ASC']]
+      Namespace.findAll({
+        attributes: ['id','namespace','uri'],
+        where: { id: namespace },
+        include: [{
+          model: Domain,
+          attributes: ['id','domain','uri'],
+          where: { id: domain },
+          order: [['id','ASC']],
+          include: [{
+              model: Attribute,
+              attributes: ['id','attribute','uri'],
+              required: false,
+              order: [['id','ASC']],
+            }]
+          }],
+          // order: [[Attribute,Domain,'id','ASC']],
         })
         .then((messages) => {
             if (messages.length === 0) {
@@ -82,23 +145,31 @@ function getAttribute(req, res, next) {
         })
         .catch(next);
     } else {
-        Namespace.findAll({
-          where: { uri: namespace },
-        }).then((namespace) => {
-          Domain.findAll({
-              where: { NamespaceId: namespace[0].dataValues.id },
+      Namespace.findAll({
+        attributes: ['id','namespace','uri'],
+        where: { uri: namespace },
+        include: [{
+          model: Domain,
+          attributes: ['id','domain','uri'],
+          where: { uri: domain },
+          order: [['id','ASC']],
+          include: [{
+              model: Attribute,
+              attributes: ['id','attribute','uri'],
+              required: false,
               order: [['id','ASC']]
-          })
-          .then((messages) => {
-              if (messages.length === 0) {
-                  const err = new APIError('There were no results', 'NO_RESULTS', httpStatus.NOT_FOUND, true);
-                  next(err);
-              } else {
-                  res.send(messages);
-              }
-          })
-          .catch(next);
+            }],
+          }],
+          // order: [[Attribute,Domain,'id','ASC']],
+        }).then((messages) => {
+            if (messages.length === 0) {
+                const err = new APIError('There were no results', 'NO_RESULTS', httpStatus.NOT_FOUND, true);
+                next(err);
+            } else {
+                res.send(messages);
+            }
         })
+        .catch(next);
     }
 }
 
